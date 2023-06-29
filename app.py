f632f1c871940db36adb08edbff19476b3f880c2
Aditya Dhandi <86340419+dhandiaditya@users.noreply.github.com>
2023-06-29 05:49:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+
 
 def submit_coupon():
     # Set up the Selenium driver
