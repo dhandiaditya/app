fe597bb5822e49812e54363ae5f278b83eff33dc
Aditya Dhandi <86340419+dhandiaditya@users.noreply.github.com>
2023-07-01 02:44:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,29 +7,29 @@ from selenium.webdriver.support import expected_conditions as EC
 def submit_coupon():
     # Define the URL of the webpage
     url = "https://www.real.discount/"
-    
+
     # Set up the Chrome driver
     chrome_driver_path = "/chromedriver.exe/"  # Replace with the actual path to chromedriver executable
     options = webdriver.ChromeOptions()
     options.add_argument("--headless")  # Run Chrome in headless mode
     driver = webdriver.Chrome(options=options)
-    
+
     # Load the webpage
     driver.get(url)
 
     # Wait for the page to load
-    wait = WebDriverWait(driver, 10)
-    wait.until(EC.presence_of_element_located((By.NAME, 'urls')))
-    
+    wait = WebDriverWait(driver, 20)  # Increase the timeout value
+    wait.until(EC.presence_of_element_located((By.ID, 'urls')))  # Use a different locator strategy if necessary
+
     # Find the text area for the coupon link and enter the link
-    textarea = driver.find_element(By.NAME, "urls")
+    textarea = driver.find_element(By.ID, "urls")  # Use a different locator strategy if necessary
     final_url = "https://www.udemy.com/course/oops-with-python-object-oriented-programming-language/?couponCode=052836015188\nhttps://www.udemy.com/course/advanced-postgresql-for-professionals/?couponCode=051227445896"
     textarea.send_keys(final_url)
-    
+
     # Submit the form
     submit_button = driver.find_element(By.NAME, 'submit1')
     submit_button.click()
-    
+
     # Close the driver
     driver.quit()
 
